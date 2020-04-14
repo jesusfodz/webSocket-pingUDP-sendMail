611f99cc5b3ee5df75a02e7b28038a66c31642ad
Jesus Fontalvo <jesusfodz@gmail.com>
2020-04-14 10:58:13 -0500

    thymeleaf configuracion

---
+++ src/main/java/co/edu/poligran/ThymeleafConfiguration.java
@@ -0,0 +1,31 @@
+package co.edu.poligran;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+@Configuration
+@AutoConfigureAfter(ThymeleafAutoConfiguration.class)
+public class ThymeleafConfiguration {
+
+	@Autowired
+	SpringTemplateEngine engine;
+
+	@PostConstruct
+	public void setTemplateREsolver() {
+		FileTemplateResolver templateResolver = new FileTemplateResolver();
+		templateResolver.setPrefix("./src/main/resources/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		templateResolver.setCacheable(false);
+		templateResolver.setCharacterEncoding("UTF-8");
+		templateResolver.setOrder(1);
+		engine.setTemplateResolver(templateResolver);
+	}
+
+}
